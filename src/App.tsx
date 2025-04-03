b37aa7ec699ebfa306829a6b0fcfaa550d3c5a82
Jerome Marbach <jeromemarbach@outlook.fr>
2025-04-03 14:53:38 +0200

    Added a condition for MEW poster

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 
-import './App.css';
+import './App.css'
 
 
-import PokemonCard from "./components/PokemonCard";
+import PokemonCard from "./components/PokemonCard"
 
 function App() {
   return (
     <div>
       <PokemonCard />
     </div>
-  );
+  )
 } 
 
-export default App;
+export default App
--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,11 +1,30 @@
- 
- function PokemonCard(){
+const pokemonList = [
+    {
+      name: "Bulbizard",
+      imgSrc:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+    },
+    {
+      name: "mew",
+      //imgSrc: "https://th.bing.com/th/id/R.05511006a68ef1f0932fb528472301d8?rik=wjUJvd2UiPAegg&riu=http%3a%2f%2fimages6.fanpop.com%2fimage%2fphotos%2f36600000%2fMew-pokemon-image-mew-pokemon-36642382-800-731.png&ehk=agviYc9S3StGrz0g9xkcekZCDW1ukNx5gFlOX1qxUPA%3d&risl=&pid=ImgRaw&r=0"
+    },
+  ];
+function PokemonCard(){
+    const pokemon = pokemonList[0]
+         
     return <figure>
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="" />
+                {pokemon.imgSrc ?(
+                <img src={pokemon.imgSrc} alt= {pokemon.name} />
+                ) : (
+                  <p>???</p>
+                )}
                 <figcaption>
-                    <p>bulbasaur</p>
+                  <p>{pokemon.name}</p>
                 </figcaption>
             </figure>
  }
-    
-  export default PokemonCard 
+  
+
+ export default PokemonCard 
+//si pokemon .imgSrc dispo affiche la
+//sinon affiche ca ???
+
